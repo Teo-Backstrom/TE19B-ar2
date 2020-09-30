7ea2b55532f3a011d703d0dc8b74c5210cbdf0c1
Teo Bäckström <teo.backstrom@learnet.se>
2020-09-30 14:51:33 +0200

    jag har gjort klar min frågesport.

--- Kapitel3/Upgift-3.6/Projekt-1/Program.cs
+++ Kapitel3/Upgift-3.6/Projekt-1/Program.cs
@@ -14,33 +14,112 @@ namespace Projekt_1
             int fingrar = int.Parse(Console.ReadLine());
             //om hen svarar rätt gå vidare
 
+            string Bearnaisesås = "";
+            string svar1 = "";
+            string svar11 = "";
+            string svar2 = "";
+            string sport = "";
+            int matte = 0;
+
+            //kontrolerar svar och frgar om man vill fortsätta
             if (fingrar == 5)
             {
                 Console.WriteLine("Rätt svar");
                 Console.WriteLine("Vill du fortsätta eller vill du stanna här och ta hem 10$?");
-                string svar1 = Console.ReadLine();
+                svar1 = Console.ReadLine();
                 svar1 = svar1.ToLower();
-
-                if (svar1 == stanna || svar1 == jag vill stanna)
-                {
-                    Console.WriteLine("Här får du dina 10$");
-                }
-
-                else if (svar1 == fortsätta || svar1 == jag vill fortsätta )
-                {
-                    Console.WriteLine("Nästa fråga är");
-                    Console.WriteLine("Nämn en ingredisens i Bearnaisesås");
-                    string Bearnaisesås = Console.ReadLine();
-                    Bearnaisesås = Bearnaisesås.ToLower();
-                }
-
             }
-            if (Bearnaisesås == smör || Bearnaisesås == schalottenlök || Bearnaisesås == fransk dragon ||
-            Bearnaisesås == vitpeppar || Bearnaisesås == rödvinsvinäger || Bearnaisesås == vitvinsvinäger ||
-            Bearnaisesås == salt || Bearnaisesås == gul lök || Bearnaisesås == äggula || Bearnaisesås == persilja
-            || Bearnaisesås == vatten)
+            //kontrolerar svar
+
+            //kontrolerar svar och ställer fråga
+            if (svar1 == "fortsätta" || svar1 == "jag vill fortsätta")
             {
-                Console.WriteLine("Rätt svar!");
+                Console.WriteLine("Nästa fråga är");
+                Console.WriteLine("Nämn en ingredisens i Bearnaisesås");
+                Bearnaisesås = Console.ReadLine();
+                Bearnaisesås = Bearnaisesås.ToLower();
+
+                //kontrolerar svar och stälelr fråga
+                if (Bearnaisesås == "smör" ||
+                Bearnaisesås == "schalottenlök" ||
+                Bearnaisesås == "fransk dragon" ||
+                Bearnaisesås == "vitpeppar" ||
+                Bearnaisesås == "rödvinsvinäger" ||
+                Bearnaisesås == "vitvinsvinäger" ||
+                Bearnaisesås == "salt" ||
+                Bearnaisesås == "gul lök" ||
+                Bearnaisesås == "äggula" ||
+                Bearnaisesås == "persilja" ||
+                Bearnaisesås == "vatten")
+                {
+                    Console.WriteLine("Rätt svar!");
+                    Console.WriteLine("Vill du fortsätta eller vill du stanna här och ta hem 20$?");
+                    svar11 = Console.ReadLine();
+                    svar11 = svar11.ToLower();
+
+                    //kontrolerar svar och ställer fråga
+                    if (svar11 == "fortsätta" || svar11 == "jag vill fortsätta")
+                    {
+                        Console.WriteLine("Vill du ha en sportfråga eller matte fråga?");
+                        svar2 = Console.ReadLine();
+                        svar2 = svar2.ToLower();
+
+                        //kontrolerar svar och ställer fråga
+                        if (svar2 == "sportfråga" || svar2 == "jag vill ha en sportfråga")
+                        {
+                            Console.WriteLine("vem har vunnit flest us open i golf?");
+                            sport = Console.ReadLine();
+                            sport = sport.ToLower();
+
+                            //kontrolerar svar och gratulerar
+                            if (sport == "tiger" || sport == "woods" || sport == "tiger woods")
+                            {
+                                Console.WriteLine("Rätt svar");
+                                Console.WriteLine("Detta var slutet av denna frågesport du har vinnit 30$");
+                            }
+                        }
+                        // kontrolerar svar och stälelr fråga
+                        else if (svar2 == "mattefråga" || svar2 == "jag vill ha en mattefråga")
+                        {
+                            Console.WriteLine("Vad är (100)^(1/2)");
+                            matte = int.Parse(Console.ReadLine());
+                        }
+                        //kontrolerar svar och gratulerar
+                        if (matte == 10)
+                        {
+                            Console.WriteLine("Rätt svar");
+                            Console.WriteLine("Detta var slutet av denna frågesport du har vinnit 30$");
+                        }
+                    }
+                    else if (svar11 == "stanna" || svar11 == "jag vill stanna här")
+                    {
+                        Console.WriteLine("Här får du dina 20$");
+                    }
+                }
+            }
+            else if (svar1 == "stanna" || svar1 == "jag vill stanna här")
+            {
+                Console.WriteLine("Här får du dina 10$");
+            }
+
+
+            //kontrolerar svar och svarar
+
+
+
+
+
+
+
+
+
+
+
+
+            //om inget stämer åvan så förlorar man
+            else
+            {
+                Console.WriteLine("sry men du klarde inte det");
             }
 
         }
