e489a234ac9261b23ab3cf6f8a04c6971ed7e897
Teo Bäckström <teo.backstrom@learnet.se>
2020-10-01 11:06:14 +0200

    Tränat p if och for loop

---
+++ Kapitel3/GissaEttTal/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GissaEttTal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //slumpa ett tal
+            Random random = new Random();
+            int slumptal = random.Next(1, 6);
+            // läs in gissningen
+
+
+
+            //fråga 5x
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("gissa et tal(1-6)");
+                int gissning = int.Parse(Console.ReadLine());
+
+                //kolla om gissningen är korrekt
+
+                if (gissning == slumptal)
+                {
+                    Console.WriteLine("Rätt");
+                    i = 5;
+                }
+                else
+                {
+                    Console.WriteLine("Fel");
+                }
+            }
+        }
+    }
+}
---
+++ Kapitel3/test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hur gör man en loop
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("loop nummer" + i);
+            }
+        }
+    }
+}
