6da4c29e2fc86938b0c20ae00f692d0e700e155a
Teo Bäckström <teo.backstrom@learnet.se>
2020-11-25 14:56:40 +0100

    tränat på array och positioner i string

---
+++ kapitel-5/kapitel-5.morse/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace kapitel_5.morse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string svar = "j";
+
+            while (svar != "n")
+            {
+                Console.WriteLine("skriv en bokstav");
+                string bokstav = Console.ReadLine();
+
+                bokstav = bokstav.ToUpper();
+
+                string alfabetet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ ";
+
+
+                int index = alfabetet.IndexOf(bokstav);
+
+                Console.WriteLine(bokstav + " finns på plats nr " + index);
+
+
+                string[] morse = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", ".--", "...-", ".--", "-..-", "-.--", "--..", ".--.-", ".-.-", "---.", " " };
+
+                string morseBokstav = morse[index];
+                Console.WriteLine(morse[index]);
+
+                Console.WriteLine("fortsätta");
+                svar = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ kapitel-5/morsemorse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace morsemorse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string alfabetet = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ ";
+
+            string[] morse = { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", ".--", "...-", ".--", "-..-", "-.--", "--..", ".--.-", ".-.-", "---.", " " };
+
+            Console.WriteLine("Prorammet översätter string till morse skriv en bokstav efter den anddra med enter mellan varje bokstav");
+            Console.WriteLine("skriv bokstav");
+
+            string meddelande = Console.ReadLine();
+
+
+
+            for (int i = 0; i < meddelande.Length; i++)
+            {
+                string bokstav = meddelande[i].ToString();
+                int index = alfabetet.IndexOf(bokstav);
+                Console.WriteLine(morse[index]);
+            }
+
+
+        }
+    }
+}
