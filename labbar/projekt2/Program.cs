d6cbcdbbb4bc7c941a48afc45b454cb84362982a
Teo Backstrom <teo.backstrom@elev.ga.ntig.se>
2021-01-21 00:07:44 +0100

    arbetat med filhantering

--- labbar/projekt2/Program.cs
+++ labbar/projekt2/Program.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using System.IO;
 
-namespace projekt2
+namespace Uppgift_P2._1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //flagga för while loop
+            bool flagga = false;
+
+            //alternativ meny
+            Console.WriteLine("dina alternativ");
+            Console.WriteLine("1 Skapa/skriva över en fil");
+            Console.WriteLine("2 Läsa in innehållet från en fil");
+
+            //inläsning av val
+            string val = Console.ReadLine();
+
+            // loop ifall man skriver fel filnamn
+            while (!flagga)
+            {
+                //skriv in namn på fil
+                Console.WriteLine("Skriv namnet på filen");
+                string filnamn = Console.ReadLine();
+
+                switch (val)
+                {
+                    case "1":
+
+                        flagga = true;
+                        Console.WriteLine("Skriv vad du vill ska komma in i filen");
+                        string rad = Console.ReadLine();
+                        //infogar de ovan i filnamnet man valt
+                        File.WriteAllText(filnamn, rad);
+                        break;
+
+                    case "2":
+                        //kollar om vilen exicterar
+                        if (File.Exists(filnamn))
+                        {
+                            flagga = true;
+
+                            //skriver ut innehållet
+                            Console.WriteLine("Här är filens innehåll:");
+                            Console.WriteLine(File.ReadAllText(filnamn));
+                        }
+                        else
+                        {
+                            //felmeddeilande får göra om
+                            flagga = false;
+                            Console.WriteLine("Filen finns inte!");
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+
+
         }
     }
 }
---
+++ labbar/sökLandICsv-fil/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+
+namespace Countries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Presentera programmet
+            Console.WriteLine("Program för att hitta ländder med matchande sökterm");
+            Console.WriteLine("Program listar land och landskod");
+
+            // Läs in alla rader från textfilen countries.csv
+            //filnamnet
+            string filnamn = "./countries.csv";
+
+            // Läs in alla rader
+            string[] rader = File.ReadAllLines(filnamn);
+
+
+            // Programloopen, avslutas med return
+
+
+
+
+            // Fråga användare om en sökterm
+            Console.WriteLine("Ange sökterm");
+            string sökterm = Console.ReadLine().ToLower();
+
+
+            // Sökning fungerar oavsett stora eller små bokstäver se https://krank23.gitbook.io/csharp-ref/grundlaeggande/string-manipulering
+
+
+
+            // Avbryt programloop användaren matat in tomt dvs return
+
+
+
+
+            // Loopa igenom alla rader
+            int counter = 1;
+            foreach (var rad in rader)
+            {
+                if (rad.Contains(sökterm))
+                {
+                    Console.WriteLine(counter + "" + rad);
+                    counter++;
+                }
+
+            }
+
+            // Dela upp raden
+
+
+            // Plocka ut land
+
+
+            // Plocka ut landskod
+
+
+            // Hitta land som innehåller sökterm se https://www.geeksforgeeks.org/c-sharp-string-contains-method/
+
+
+            // Skriv ut matchande land och dess landskod
+
+
+        }
+    }
+}
+
+// Summering
+
