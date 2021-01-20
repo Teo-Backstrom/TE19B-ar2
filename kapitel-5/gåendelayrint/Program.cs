0ea9481a7c6b1f6ad8348c6039e45364b33c0046
Teo Backstrom <teo.backstrom@elev.ga.ntig.se>
2021-01-20 13:49:50 +0100

    repetion av switch arrayer slump try parse + datum

--- kapitel-5/gåendelayrint/Program.cs
+++ kapitel-5/gåendelayrint/Program.cs
@@ -1,115 +1,128 @@
 ﻿using System;
 
-namespace labyrint
+namespace Labyrint
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //För att skriva ut enteties
-            Console.OutputEncoding = System.Text.Encoding.Default;
 
-            // Skapa en matris med strängar
-            int[,] karta = {
-                {1, 1, 1, 1, 1, 1, 1},
-                {1, 0, 0, 0, 0, 0, 1},
-                {1, 0, 1, 1, 1, 0, 1},
-                {1, 0, 1, 1, 0, 0, 1},
-                {1, 0, 1, 1, 0, 1, 1},
-                {1, 0, 0, 0, 0, 0, 1},
-                {1, 0, 1, 1, 1, 1, 1}
+            int poang = 0;
+            // För att skriva ut Entities
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+            // Slumpa fram in en tomte i labyrinten
+            Random slumpMotor = new Random();
+
+            // Skapa en labyrint
+            int[,] labyrint = {
+                {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},  // 0
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},  // 1
+                {1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1},  // 2
+                {1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1},  // 3
+                {1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1},  // 4
+                {1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1},  // 5
+                {1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 1},  // 6
+                {1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1},  // 7
+                {1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1},  // 8
+                {1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1}   // 9
             };
-            //slumpa fram in en figur i labyrinten
-            Random slumpmotor = new Random();
+            //var är spelare
+            int spelarePosX = 1;
+            int spelarePosY = 9;
+            // Placera ut tomtar
+            int antalTomtar = 3;
+            while (antalTomtar != 0)
+            {
+                // Slumpa fram koordinater
+                int x = slumpMotor.Next(0, 11);  // 0-10
+                int y = slumpMotor.Next(0, 10);  // 0-9
+
+                // Hamnar figuren i en gång?
+                if (labyrint[y, x] == 0)
+                {
+                    labyrint[y, x] = 2;
+                    antalTomtar--;
+
+                }
+            }
+
+
+
+            //Spellopen
             while (true)
             {
-                // slumpa fram kordinater
-                int x = slumpmotor.Next(0, 7);
-                int y = slumpmotor.Next(0, 7);
-
-                //manar vi i en gång
-                if (karta[y, x] == 0)
+                Console.Clear();
+                //infoga spelaren
+                labyrint[spelarePosY, spelarePosX] = 3;
+                Console.WriteLine(poang);
+                // Skriva ut labyrinten
+                // Loopa igenom rad för rad, dvs i y-led
+                for (int y = 0; y < 10; y++)
                 {
-                    karta[y, x] = 2;
-                    break;
-                }
-
-            }
-
-
-
-            //skriva ut labyrinten
-            //loopa igenom rad för rad
-            for (int y = 0; y < 7; y++)
-            {
-                for (int x = 0; x < 7; x++)
-                {
-                    switch (karta[y, x])
+                    // Loopa igenom kolumnvis, dvs x-led
+                    for (int x = 0; x < 11; x++)
                     {
-                        case 1:
-                            Console.Write('\u2B1C');
-                            break;
+                        switch (labyrint[y, x])
+                        {
+                            case 1: // En vägg
+                                Console.Write('\u2B1C');
+                                break;
 
-                        case 2:
-                            Console.Write("⏺ ");
-                            break;
+                            case 2: // En tomte
+                                Console.Write("0 ");
+                                break;
 
-                        default:
-                            Console.Write('\u2B1C');
-                            break;
+                            case 3: // En spelare
+                                Console.Write("# ");
+                                break;
+
+                            default: // En gång
+                                Console.Write('\u2B1B');
+                                break;
+                        }
                     }
-
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
-            }
-            int x = slumpmotor.Next(0, 7);
-            int y = slumpmotor.Next(0, 7);
-            Console.WriteLine("hur vill du gå");
-            string inmatning = Console.ReadLine();
 
-            switch (inmatning)
-            {
-                case "w":
-                    y--;
-                    break;
+                //frpga användren vart spelaren ska gå
+                Console.WriteLine("Vart vill du gå?(wasd)");
+                // läas in omvandal till stora bokstäver och ta första tecknet.
+                string riktning = Console.ReadLine().ToUpper().Substring(0, 1);
 
-                case "s":
-                    y++;
-                    break;
-
-                case "a":
-                    x--;
-                    break;
-
-                case "d":
-                    x++;
-                    break;
-                default:
-            }
-            //skriva ut labyrinten
-            //loopa igenom rad för rad
-            for (int y = 0; y < 7; y++)
-            {
-                for (int x = 0; x < 7; x++)
+                //radera position spelaren
+                labyrint[spelarePosY, spelarePosX] = 0;
+                switch (riktning)
                 {
-                    switch (karta[y, x])
-                    {
-                        case 1:
-                            Console.Write('\u2B1C');
-                            break;
-
-                        case 2:
-                            Console.Write("⏺ ");
-                            break;
-
-                        default:
-                            Console.Write('\u2B1C');
-                            break;
-                    }
-
+                    case "A":
+                        if (labyrint[spelarePosY, spelarePosX - 1] != 1)
+                        {
+                            spelarePosX--;
+                        }
+                        break;
+                    case "D":
+                        spelarePosX++;
+                        break;
+                    case "W":
+                        spelarePosY--;
+                        break;
+                    case "S":
+                        spelarePosY++;
+                        break;
+                    default:
+                        Console.WriteLine("Förstod inte vad du menar. Vg försök igen");
+                        break;
                 }
-                Console.WriteLine();
+
+                if (labyrint[spelarePosY, spelarePosX] == 2)
+                {
+                    poang++;
+                }
+
+
+
             }
+
         }
     }
 }
