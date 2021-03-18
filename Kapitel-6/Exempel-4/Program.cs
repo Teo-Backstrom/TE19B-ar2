b1d53406be360a7b2b6467706241d9780749253c
Teo Backstrom <teo.backstrom@elev.ga.ntig.se>
2021-03-18 11:06:20 +0100

    jobbat med class varibaler och blanade uppifter

---
+++ Kapitel-6/Exempel-4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exempel_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal = ReadInt();
+            Console.WriteLine(tal + 1);
+        }
+
+        static int ReadInt()
+        {
+            int heltal;
+            while (!int.TryParse(Console.ReadLine(), out heltal))
+            {
+                Console.WriteLine("Du skrev inte in ett heltal. Försök igen.");
+            }
+            return heltal;
+        }
+    }
+}
